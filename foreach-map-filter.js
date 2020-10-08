7dd70b977aaf9f658f6c2c25e4f6b83a7ca69439
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-08 14:50:39 -0500

    Passes all tests

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -6,8 +6,14 @@ Examples:
     doubleValues([5,1,2,3,10]) // [10,2,4,6,20]
 
 */
-function doubleValues(arr){
-    
+function doubleValues(arr) {
+  const arrDouble = [];
+  arr.forEach(function (val) {
+    let done = val * 2;
+    arrDouble.push(done);
+  });
+
+  return arrDouble;
 }
 
 /*
@@ -18,8 +24,15 @@ Examples:
     onlyEvenValues([5,1,2,3,10]) // [2,10]
 
 */
-function onlyEvenValues(arr){
-    
+function onlyEvenValues(arr) {
+  const arrEven = [];
+  arr.forEach(function (val) {
+    if (val % 2 === 0) {
+      arrEven.push(val);
+    }
+  });
+
+  return arrEven;
 }
 
 /*
@@ -30,8 +43,14 @@ Examples:
     showFirstAndLast(['hi', 'goodbye', 'smile']) // ['hi', 'ge', 'se']
 
 */
-function showFirstAndLast(arr){
-    
+function showFirstAndLast(arr) {
+  const arr2 = [];
+  arr.forEach(function (val) {
+    let done = val[0] + val[val.length - 1];
+    arr2.push(done);
+  });
+
+  return arr2;
 }
 
 /*
@@ -43,8 +62,12 @@ Examples:
     // [{name: 'Elie', title:'instructor'}, {name: 'Tim', title:'instructor'}, {name: 'Matt', title:'instructor'}, {name: 'Colt', title:'instructor'}]
 
 */
-function addKeyAndValue(arr,key,value){
-    
+function addKeyAndValue(arr, key, value) {
+  arr.forEach(function (val) {
+    val[key] = value;
+  });
+
+  return arr;
 }
 
 /*
@@ -57,8 +80,21 @@ Examples:
     vowelCount('hmmm') // {};
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
-function vowelCount(str){
-   
+function vowelCount(str) {
+  return str.split("").reduce(function (accumulator, value) {
+    var val = value.toLowerCase();
+    if (['a', 'e', 'i', 'o', 'u'].indexOf(val) > -1) {
+      if (val in accumulator) {
+        accumulator[val]++;
+      } else {
+        accumulator[val] = 1;
+      }
+    }
+
+    return accumulator;
+  }, {
+
+  });
 }
 
 /*
@@ -69,7 +105,13 @@ Examples:
     doubleValuesWithMap([1,-2,-3]) // [2,-4,-6]
 */
 
-function doubleValuesWithMap(arr) {}
+function doubleValuesWithMap(arr) {
+  const mapped = arr.map(function (val, idx, arr) {
+    return val * 2;
+  });
+
+  return mapped;
+}
 
 /*
 Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
@@ -79,8 +121,12 @@ Examples:
     valTimesIndex([1,-2,-3]) // [0,-2,-6]
 */
 
-function valTimesIndex(arr){
-    
+function valTimesIndex(arr) {
+  const mapped = arr.map(function (val, idx, arr) {
+    return val * idx;
+  });
+
+  return mapped;
 }
 
 /*
@@ -90,8 +136,10 @@ Examples:
     extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractKey(arr, key){
-    
+function extractKey(arr, key) {
+  return arr.map(function (val, idx, arr) {
+    return val[key];
+  });
 }
 
 /*
@@ -101,8 +149,10 @@ Examples:
     extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
 */
 
-function extractFullName(arr){
-    
+function extractFullName(arr) {
+  return arr.map(function (val, idx, arr) {
+    return val.first + ' ' + val.last;
+  });
 }
 
 /*
@@ -112,7 +162,11 @@ Examples:
     filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
-function filterByValue(arr, key) {}
+function filterByValue(arr, key) {
+  return arr.filter(function (val) {
+    return val[key] != undefined;
+  });
+}
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -122,7 +176,11 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) {}
+function find(arr, searchValue) {
+  return arr.filter(function (val) {
+    return val == searchValue;
+  })[0];
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
@@ -131,7 +189,11 @@ Examples:
     findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
 */
 
-function findInObj(arr, key, searchValue) {}
+function findInObj(arr, key, searchValue) {
+  return arr.filter(function (val) {
+    return val[key] === searchValue;
+  })[0];
+}
 
 /*
 Write a function called removeVowels which accepts a string and returns a new string with all of the vowels (both uppercased and lowercased) removed. Every character in the new string should be lowercased.
@@ -142,7 +204,13 @@ Examples:
     removeVowels('ZZZZZZ') // ('zzzzzz')
 */
 
-function removeVowels(str) {}
+function removeVowels(str) {
+  const vowels = 'aeiou';
+  return str.toLowerCase().split('').filter(function (val) {
+      return vowels.indexOf(val) === -1;
+    })
+    .join('')
+}
 
 /*
 Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and filter to double and then filter the odd numbers).
@@ -152,4 +220,11 @@ Examples:
     doubleOddNumbers([4,4,4,4,4]) // []
 */
 
-function doubleOddNumbers(arr) {}
+function doubleOddNumbers(arr) {
+  return arr.filter(function (val) {
+      return val % 2 !== 0;
+    })
+    .map(function (val) {
+      return val * 2;
+    });
+}
